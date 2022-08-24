dadb5db93a97ecffc5862ca2315e3361bbc6becd
iacobo <47821203+iacobo@users.noreply.github.com>
2022-08-24 14:21:48 +0800

    Fix flatten.

--- nets/nets.py
+++ nets/nets.py
@@ -108,7 +108,7 @@ class SimpleCNN(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = x.flatten(start_dim=1)
         x = self.classifier(x)
         return x
 
