48e7de0b3929972c6b0790aaa0626595b325cff7
iacobo <47821203+iacobo@users.noreply.github.com>
2022-08-26 11:08:14 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,6 +13,109 @@ import matplotlib.pyplot as plt
 from torchviz import make_dot
 
 
+def get_device():
+    """
+    Returns:
+        torch.device: Torch device. First GPU if available, else CPU.
+    """
+    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def render_model(lat_mb_x, model, mb_x, train_exp_counter):
+    """
+    Renders graph of model.
+    """
+    make_dot(
+        model(mb_x, latent_input=lat_mb_x, return_lat_acts=True),
+        params=dict(
+            list(model.named_parameters())
+            + [("mb_x", mb_x)]
+            + ([] if train_exp_counter == 0 else [("lat_mb_x", lat_mb_x)])
+        ),
+    ).render(f"torchviz_output_exp{train_exp_counter}", format="png")
+
+
+def plot_random_example():
+    """Plots random examples from each class / dist.
+    """
+
+    raise NotImplementedError
+
+
+def plot_results(
+    results,
+    method_name,
+    ax,
+    n_experiences,
+    metric="acc",
+    mode="train",
+    repeat_vals=False,
+):
+
+    results_clean = {"train": {"acc": [], "loss": []}, "test": {"acc": [], "loss": []}}
+    loss_prefix = f"Loss_Stream/eval_phase/{mode}_stream/"
+    acc_prefix = f"Top1_Acc_Stream/eval_phase/{mode}_stream/"
+
+    results_clean[mode]["loss"] = [
+        [result[f"{loss_prefix}Task{str(i).zfill(3)}"] for result in results]
+        for i in range(n_experiences)
+    ]
+    results_clean[mode]["acc"] = [
+        [result[f"{acc_prefix}Task{str(i).zfill(3)}"] for result in results]
+        for i in range(n_experiences)
+    ]
+
+    res = results_clean[mode][metric]
+
+    if repeat_vals:
+        res = [list(np.repeat(val, repeat_vals)) for val in res]
+
+    for i in range(n_experiences):
+        ax.plot(res[i], label=f"Task {i}")
+
+    ax.set_title(f"{method_name} {mode.capitalize()} {metric.capitalize()}")
+
+    return results_clean
+
+
+def plot_single_legend(fig):
+    """
+    For multiple subplots with shared labeled lines to plot, 
+    combines legends to remove redundancy.
+    """
+    labels_handles = {
+        label: handle
+        for ax in fig.axes
+        for handle, label in zip(*ax.get_legend_handles_labels())
+    }
+
+    fig.legend(
+        labels_handles.values(),
+        labels_handles.keys(),
+        loc="upper center",
+        bbox_to_anchor=(0.5, 0),
+        bbox_transform=plt.gcf().transFigure,
+    )
+
+
+def train_gmm(n_epochs, x, lr=0.001, momentum=0.9):
+
+    parameters = [weights, means, stdevs]
+    optimizer1 = optim.SGD(parameters, lr=lr, momentum=momentum)
+    gmm = GMM()
+
+    for i in range(n_epochs):
+        optimizer1.zero_grad()
+        x = torch.randn(5000, 2)  # this can be an arbitrary x samples
+        loss2 = -gmm.log_prob(x).mean()  # -densityflow.log_prob(inputs=x).mean()
+        loss2.backward()
+        optimizer1.step()
+
+        print(i, loss2)
+
+    return None
+
+
 class GMM(nn.Module):
     def __init__(self, n_components, dim, weights):
         """
@@ -43,127 +146,4 @@ class GMM(nn.Module):
         gmm = D.MixtureSameFamily(mix, comp)
 
         return gmm
-
-
-def plot_results(
-    results,
-    method_name,
-    ax,
-    n_experiences,
-    metric="acc",
-    mode="train",
-    repeat_vals=False,
-):
-    results_clean = {"train": {"acc": [], "loss": []}, "test": {"acc": [], "loss": []}}
-    loss_prefix = f"Loss_Stream/eval_phase/{mode}_stream/"
-    acc_prefix = f"Top1_Acc_Stream/eval_phase/{mode}_stream/"
-
-    results_clean[mode]["loss"] = [
-        [result[f"{loss_prefix}Task{str(i).zfill(3)}"] for result in results]
-        for i in range(n_experiences)
-    ]
-    results_clean[mode]["acc"] = [
-        [result[f"{acc_prefix}Task{str(i).zfill(3)}"] for result in results]
-        for i in range(n_experiences)
-    ]
-
-    res = results_clean[mode][metric]
-
-    if repeat_vals:
-        res = [list(np.repeat(val, repeat_vals)) for val in res]
-
-    for i in range(n_experiences):
-        ax.plot(res[i], label=f"Task {i}")
-
-    ax.set_title(f"{method_name} {mode.capitalize()} {metric.capitalize()}")
-
-    return results_clean
-
-
-def plot_single_legend(fig):
-    labels_handles = {
-        label: handle
-        for ax in fig.axes
-        for handle, label in zip(*ax.get_legend_handles_labels())
-    }
-
-    fig.legend(
-        labels_handles.values(),
-        labels_handles.keys(),
-        loc="upper center",
-        bbox_to_anchor=(0.5, 0),
-        bbox_transform=plt.gcf().transFigure,
-    )
-
-
-def plot_random_example():
-    """Plots random examples from each class / dist.
-    """
-
-    return None
-
-
-def train_gmm(n_epochs, x, lr=0.001, momentum=0.9):
-
-    parameters = [weights, means, stdevs]
-    optimizer1 = optim.SGD(parameters, lr=lr, momentum=momentum)
-    gmm = GMM()
-
-    for i in range(n_epochs):
-        optimizer1.zero_grad()
-        x = torch.randn(5000, 2)  # this can be an arbitrary x samples
-        loss2 = -gmm.log_prob(x).mean()  # -densityflow.log_prob(inputs=x).mean()
-        loss2.backward()
-        optimizer1.step()
-
-        print(i, loss2)
-
-    return None
-
-
-def get_device():
-    """
-    Returns:
-        torch.device: Torch device. First GPU if available, else CPU.
-    """
-    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-# JA: copy of Avalanche funcs
-
-
-class LayerAndParameter(NamedTuple):
-    layer_name: str
-    layer: Module
-    parameter_name: str
-    parameter: Tensor
-
-
-def get_layers_and_params(model: Module, prefix="") -> List[LayerAndParameter]:
-    result: List[LayerAndParameter] = []
-    for param_name, param in model.named_parameters(recurse=False):
-        result.append(LayerAndParameter(prefix[:-1], model, prefix + param_name, param))
-
-    layer_name: str
-    layer: Module
-    for layer_name, layer in model.named_modules():
-        if layer == model:
-            continue
-
-        layer_complete_name = prefix + layer_name + "."
-
-        result += get_layers_and_params(layer, prefix=layer_complete_name)
-
-    return result
-
-
-def render_model(lat_mb_x, model, mb_x, train_exp_counter):
-    make_dot(
-        model(mb_x, latent_input=lat_mb_x, return_lat_acts=True),
-        params=dict(
-            list(model.named_parameters())
-            + [("mb_x", mb_x)]
-            + ([] if train_exp_counter == 0 else [("lat_mb_x", lat_mb_x)])
-        ),
-    ).render(f"torchviz_output_exp{train_exp_counter}", format="png")
 
