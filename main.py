def331711b1fbdb53be22ea68cdb3a5d751eedef
iacobo <47821203+iacobo@users.noreply.github.com>
2022-11-09 10:57:20 +0000

    Add main script

---
+++ main.py
@@ -0,0 +1,161 @@
+# ## Generative Latent Replay
+# Experimental code to test GLRon benchmark continual learning problems.
+# i.e. normalising bottleneck representations and sampling from
+# fitted GMM on latent space.
+
+import argparse
+from pathlib import Path
+
+# ML imports
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+from avalanche.benchmarks.classic import PermutedMNIST
+
+# Local imports
+from src import utils, models
+
+# Continual Learning strategies
+from avalanche.training import Naive, Replay, plugins
+from src.strategies import LatentReplay, GenerativeLatentReplay
+
+# Setup
+
+
+def main(args):
+    # Reproducibility
+    utils.set_seed(args.SEED)
+
+    # Reporting
+    eval_rate = 1
+
+    # Problem definition
+    # Number of tasks
+    n_experiences = 3
+
+    # Transform data to format expected by model
+    transform = utils.get_transforms(resize=244, n_channels=3, normalise=True)
+
+    # Load dataset
+    experiences = PermutedMNIST(
+        n_experiences=n_experiences,
+        train_transform=transform,
+        eval_transform=transform,
+        seed=args.SEED,
+        # rotations_list=[0, 60, 300],
+    )
+
+    # Train and test streams
+    train_stream = experiences.train_stream
+    # test_stream = experiences.test_stream
+
+    # Hyperparameters
+
+    # Replays
+    replay_buffer_size = 6000
+
+    # Frozen backbone
+    freeze_depth = 5
+    latent_layer_number = freeze_depth * 3 + 1
+
+    # SGD hyperparams
+    sgd_kwargs = {
+        "lr": 0.001,  # 0.1,  # 0.001
+        "momentum": 0.9,
+        "weight_decay": 0.0005,  # l2 regularization
+    }
+
+    strategy_kwargs = {
+        "eval_every": eval_rate,
+        "train_epochs": 40,
+        "train_mb_size": 64,
+        "eval_mb_size": 128,
+        "device": utils.get_device(),
+        "plugins": [
+            plugins.EarlyStoppingPlugin(
+                patience=eval_rate,
+                val_stream_name="train_stream/Task000",
+                margin=0.003,  # metric
+            )
+        ],
+    }
+
+    # Building base model
+    model = models.alexnet()
+
+    # Loading Continual Learning strategies for experiments
+    # Training loop
+
+    if args.strategy == "latent replay":
+        strategy = LatentReplay(
+            model=model,
+            rm_sz=replay_buffer_size,
+            latent_layer_num=latent_layer_number,
+            evaluator=utils.get_eval_plugin("Latent Replay"),
+            **strategy_kwargs,
+            **sgd_kwargs,
+        )
+
+    elif args.strategy == "glr":
+        # Loading GLR model
+        strategy = GenerativeLatentReplay(
+            model=model,
+            rm_sz=replay_buffer_size,
+            latent_layer_num=latent_layer_number,
+            evaluator=utils.get_eval_plugin("GLR"),
+            **strategy_kwargs,
+            **sgd_kwargs,
+        )
+
+    elif args.strategy == "naive":
+        # Loading baseline (naive) model
+        strategy = Naive(
+            model=model,
+            optimizer=SGD(model.parameters(), **sgd_kwargs),
+            evaluator=utils.get_eval_plugin("Naive"),
+            **strategy_kwargs,
+        )
+
+    elif args.strategy == "replay":
+        # Loading benchmark (replay) model
+        strategy = Replay(
+            model=model,
+            criterion=CrossEntropyLoss(),
+            optimizer=SGD(model.parameters(), **sgd_kwargs),
+            evaluator=utils.get_eval_plugin("Replay"),
+            **strategy_kwargs,
+        )
+
+    folders = {
+        "GLR": "GLR",
+        "latent replay": "Latent Replay",
+        "naive": "Naive",
+        "replay": "Replay",
+    }
+
+    for train_exp in train_stream:
+        strategy.train(train_exp, eval_streams=[train_exp])
+        strategy.eval(train_stream)
+        utils.save_model(
+            strategy.model,
+            Path(f"results/{folders[args.strategy]}"),
+            f"model_{train_exp.current_experience}.pt",
+        )
+
+    # plotting.plot_multiple_results()
+    # plotting.results_to_df()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--strategy",
+        type=str,
+        default="glr",
+        help="Strategy to use",
+        choices=["latent replay", "glr", "naive", "replay"],
+    )
+    parser.add_argument("--experiment", type=str, default="PermutedMNIST")
+    parser.add_argument("--model", type=str, default="alexnet")
+    parser.add_argument("--SEED", type=int, default=43769)
+    args = parser.parse_args()
+    main(args)
